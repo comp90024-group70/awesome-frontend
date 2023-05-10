06263df482a34b637032898702b3b6a36f08be6b
Zhichen Ren <zhichenr@student.unimelb.edu.au>
2023-05-10 17:48:02 +1000

    change the total frameworks and add initial tools

--- visual-analysis/visual-analysis-web/src/App.js
+++ visual-analysis/visual-analysis-web/src/App.js
@@ -1,37 +1,11 @@
-
-import Home from "./Home"
-import { useEffect, useState } from 'react'
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
-import axios from 'axios'
+import Header from '../src/components/Header'
 function App () {
-  const [data, setData] = useState(null)
-  useEffect(() => {
-    //发送网络请求
-    async function loadData () {
-      //     const res = await fetch('url')// axios
-      axios.get('http://172.26.131.199:8000/api/v1/database_status').then(res => {
-        // data = res.data
-        // console.log(res.data)
-        setData(res.data.status)
-      })
-    }
-    loadData()
-  }, [])
+
+
   return (
-    <BrowserRouter>
-      {/*点击跳转位置*/}
-      <Link to='/'>Main</Link>
-      <Link to='/home'>Home</Link>
-      {/*路由出口位置*/}
-      <Routes>
-        <Route path='/home' element={<Home />}></Route>
+    <Header></Header>
 
-      </Routes>
 
-      <div>
-        {data}
-      </div>
-    </BrowserRouter>
   )
 }
 
