cf96808e93716fbcd693d46d14b9a5238c528ff7
Zhichen Ren <zhichenr@student.unimelb.edu.au>
2023-05-16 19:46:46 +1000

    question

--- visual-analysis/visual-analysis-web/src/components/LeftBox.js
+++ visual-analysis/visual-analysis-web/src/components/LeftBox.js
@@ -6,44 +6,114 @@ import axios from 'axios'
 
 
 
-
 function LeftBox () {
+
+  const [data, setData] = useState([])
+  const [country, setCountry] = useState([])
+  const [sentiment, setSentiment] = useState([])
+  const [uniqueCountry, setUniqueCountry] = useState(new Set())
+  const [dic, setDic] = useState({})
+  const newSet = new Set(uniqueCountry)
+
+  const newSentiments = []
+  const newCountries2 = []
+  //初始化
+  const initialDictionary = {}
+  uniqueCountry.forEach((key) => {
+    initialDictionary[key] = 0
+  })
+
+  //获取数据从url路径
+  useEffect(() => {
+    const interval = setInterval(() => {
+      axios.get('http://172.26.134.10:8000/api/v1/sentiment').then(res => {
+
+        setData(res.data.data)
+      })
+    }, 1000)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+
+
+  //设置x轴和y轴的值 处理数据
+  useEffect(() => {
+    const newCountries = []
+
+    data.map((item) => {
+      newCountries.push(item.gcc)
+    })
+    //收到20万条数据
+    setCountry(newCountries)
+
+    //处理数据
+  })
+
+  useEffect(() => {
+    //获取国家的唯一值
+    newSet.add(country)
+    setUniqueCountry(newSet)
+
+    data.map((item) => {
+      Object.entries(item).forEach(([key, value]) => {
+        let falg = ''
+        uniqueCountry.forEach((key2) => {
+          if (key === "gcc" && value === key2) {
+            falg = key2
+          }
+          if (key == "sentiment") {
+            initialDictionary[falg] += value
+          }
+        })
+      })
+    })
+
+    setDic(initialDictionary)
+
+
+    Object.entries(dic).forEach(([key, value]) => {
+
+      newCountries2.push(key)
+      newSentiments.push(value / 200000)//计算 20万条数据的平均值
+    })
+
+    //制作x轴和y轴的值
+    setCountry(newCountries2)
+    setSentiment(newSentiments)
+  })
+
+
+
+
+  //写入表格
   const getOption = () => {
     return {
       tooltip: {
         trigger: 'axis'
       },
       xAxis: {
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        type: 'category',
+        data: country
       },
-      yAxis: {},
-      series: [{
-        name: 'Sales',
-        type: 'bar',
-        data: [820, 932, 901, 934, 1290, 1330, 1320]
-      }]
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          type: 'line',
+          data: sentiment
+        }
+      ]
+
     }
   }
-  const [data, setData] = useState(null)
-  useEffect(() => {
-    const interval = setInterval(() => {
-      axios.get('http://172.26.136.13:8000/api/v1/mas_count').then(res => {
-        // data = res.data
-        // console.log(res.data)
-        setData(res.data.null)
-      })
-
-    }, 1000)
-    return () => {
-      clearInterval(interval)  // 组件卸载时清除间隔
-    }
-  }, [])
-
 
   return (
     <div className="leftBox">
       <div className="panelL">
-        <h2>{data}</h2>
+        <h2>avg sentiment</h2>
         <div className="chartL">
 
           < ReactECharts option={getOption()} style={{ height: "100%", width: "100%" }} />
