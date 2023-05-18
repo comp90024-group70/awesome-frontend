b23d205f9ca90e95765db94423666bc569ddcb85
Zhichen Ren <zhichenr@student.unimelb.edu.au>
2023-05-18 14:36:08 +1000

    Add maps

--- visual-analysis/visual-analysis-web/src/components/LeftBox.js
+++ visual-analysis/visual-analysis-web/src/components/LeftBox.js
@@ -1,131 +1,20 @@
 import "../css/leftBox.css"
-import React, { useState, useEffect } from 'react'
-import ReactECharts from 'echarts-for-react'
-import axios from 'axios'
+import { useContext, useEffect } from 'react'
+import { PassedFlag } from '../utils/Map'
+import { StateBarChart } from "../assets/StateBarChart"
 
 
 
 
-function LeftBox () {
 
-  const [data, setData] = useState([])
-  const [country, setCountry] = useState([])
-  const [sentiment, setSentiment] = useState([])
-  // const [uniqueCountry, setUniqueCountry] = useState(new Set())
-  // const [dic, setDic] = useState({})
-  // const newSet = new Set(uniqueCountry)
-
-  // const newSentiments = []
-  // const newCountries2 = []
-  //// 初始化
-  // const initialDictionary = {}
-  // uniqueCountry.forEach((key) => {
-  //   initialDictionary[key] = 0
-  // })
-
-  //获取数据从url路径
+function LeftBox (props) {
+  const flag = useContext(PassedFlag)
+  const { val } = props
   useEffect(() => {
-    const interval = setInterval(() => {
-      axios.get('http://172.26.136.13:8000/api/v1/sentiment').then(res => {
-
-        setData(res.data.data)
-      })
-    }, 1000)
-    return () => {
-      clearInterval(interval)
-    }
-  }, [])
-
-  //设置x轴和y轴的值 处理数据
-  useEffect(() => {
-    const newCountries = []
-    const newSentiments = []
-    data.map((item) => {
-      newCountries.push(item.gcc)
-      newSentiments.push(item.sentiment_avg)
-    })
-    //收到20万条数据
-    setCountry(newCountries)
-    setSentiment(newSentiments)
-  }, [data])
-
-  // //设置x轴和y轴的值 处理数据
-  // useEffect(() => {
-  //   const newCountries = []
-
-  //   data.map((item) => {
-  //     newCountries.push(item.gcc)
-  //   })
-  //   //收到20万条数据
-  //   setCountry(newCountries)
-
-  //   //处理数据
-  // }, [])
-
-  // useEffect(() => {
-  //   //获取国家的唯一值
-  //   newSet.add(country)
-  //   setUniqueCountry(newSet)
-
-  //   data.map((item) => {
-  //     Object.entries(item).forEach(([key, value]) => {
-  //       let falg = ''
-  //       uniqueCountry.forEach((key2) => {
-  //         if (key === "gcc" && value === key2) {
-  //           falg = key2
-  //         }
-  //         if (key == "sentiment") {
-  //           initialDictionary[falg] += value
-  //         }
-  //       })
-  //     })
-  //   })
-
-  //   setDic(initialDictionary)
+    console.log(val)
+  }, [val])
 
 
-  //   Object.entries(dic).forEach(([key, value]) => {
-
-  //     newCountries2.push(key)
-  //     newSentiments.push(value / 200000)//计算 20万条数据的平均值
-  //   })
-
-  //   //制作x轴和y轴的值
-  //   setCountry(newCountries2)
-  //   setSentiment(newSentiments)
-  // }, [])
-
-
-
-
-  //写入表格
-  const getOption = () => {
-    return {
-      tooltip: {
-        trigger: 'axis'
-      },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-      },
-      xAxis: {
-        type: 'category',
-        data: country
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [
-        {
-          type: 'bar',
-          data: sentiment
-        }
-      ]
-
-    }
-  }
 
   return (
     <div className="leftBox">
@@ -133,8 +22,7 @@ function LeftBox () {
         <h2>avg sentiment</h2>
         <div className="chartL">
 
-          < ReactECharts option={getOption()} style={{ height: "100%", width: "100%" }} />
-
+          <StateBarChart />
 
         </div>
         <div className="panelfooterL"></div>
--- visual-analysis/visual-analysis-web/src/components/MainBox.js
+++ visual-analysis/visual-analysis-web/src/components/MainBox.js
@@ -2,16 +2,20 @@ import "../css/mainBox.css"
 import LeftBox from "./LeftBox"
 import MidBox from "./MidBox"
 import RightBox from "./RightBox"
+
 function MainBox () {
 
   return (
+
     <div className="mainBox">
+
       <LeftBox />
       <MidBox />
       <RightBox />
 
 
     </div>
+
   )
 };
 
