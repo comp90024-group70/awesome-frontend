66da66453370724125d7d37391aebf5dded62833
zian.wang <913248383@qq.com>
2023-05-21 01:28:52 +1000

    Config domain

--- visual-analysis/visual-analysis-web/src/utils/requests.js
+++ visual-analysis/visual-analysis-web/src/utils/requests.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const getDomain = () => {
-    let domain = process.env.REQUEST_DOMAIN;
+    let domain = process.env.REACT_APP_REQUEST_DOMAIN;
+    console.log(domain)
     if (domain === undefined) {
-        // domain = '172.26.131.154';
-        domain = process.env.REACT_APP_REQUEST_DOMAIN;
+        domain = '172.26.131.154';
+        // domain = process.env.REACT_APP_REQUEST_DOMAIN;
     }
     return domain;
 }
